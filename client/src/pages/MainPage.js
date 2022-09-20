d2651b248cba93447d272bda2dec99369ad9904a
Tucker Whitaker <trw2005.50@gmail.com>
2022-09-20 15:00:12 -0500

    build13

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -2,7 +2,7 @@ import axios, { Axios } from "axios";
 import React, { useState, useEffect } from "react";
 import "./MainPage.css";
 const { io } = require("socket.io-client");
-var socket = io.connect("wss://whitakert.com:443/wss");
+var socket = io("wss://whitakert.com:443/wss");
 console.log(socket);
 function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
