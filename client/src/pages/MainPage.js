310f116c454b88e21865de186811efcb2845efe2
Tucker Whitaker <trw2005.50@gmail.com>
2022-09-29 13:34:50 -0500

    debugging mobile keypress2

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -196,8 +196,8 @@ function MainPage() {
 }
 
 document.addEventListener("keyup", function (event) {
-  socket.emit("keycodepressed", event.code);
-  console.log(event.code);
+  socket.emit("keycodepressed", event);
+  console.log(event);
 
   if (event.code === "Enter") {
     console.log("enter");
